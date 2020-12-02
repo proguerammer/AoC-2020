7b2f2e182c851589ce98fcb4697a0f9cf0edd3fc
Mike O'Connor <mike_oconnor@outlook.com>
2020-12-02 00:25:25 -0600

    Day 1

---
+++ Day1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+
+namespace AoC
+{
+	public static class Day1
+	{
+		public static void Solve()
+		{
+			var lines = File.ReadAllLines(@"..\..\data\day1.txt");
+			//CheckForTwo(lines);
+			CheckForThree(lines);
+		}
+
+		static void CheckForTwo(string[] lines)
+		{
+			for (int i = 0; i < lines.Length; ++i)
+			{
+				int v1 = int.Parse(lines[i]);
+				for (int j = i + 1; j < lines.Length; ++j)
+				{
+					int v2 = int.Parse(lines[j]);
+					if ((v1 + v2) == 2020)
+					{
+						Console.WriteLine(v1 * v2);
+					}
+				}
+			}
+		}
+
+		static void CheckForThree(string[] lines)
+		{
+			for (int i = 0; i < lines.Length; ++i)
+			{
+				int v1 = int.Parse(lines[i]);
+				for (int j = i + 1; j < lines.Length; ++j)
+				{
+					int v2 = int.Parse(lines[j]);
+					for (int k = j + 1; k < lines.Length; ++k)
+					{
+						int v3 = int.Parse(lines[k]);
+						if ((v1 + v2 + v3) == 2020)
+						{
+							Console.WriteLine(v1 * v2 * v3);
+						}
+					}
+
+				}
+			}
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AoC
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Day1.Solve();
+		}
+	}
+}
